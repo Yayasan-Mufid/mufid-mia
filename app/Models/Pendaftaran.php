264cbacd97ba7561e0efd10407ec9e0541bc2bcc
asif~gagang <rimbaborne@gmail.com>
2022-01-22 18:18:56 +0800

    database model

---
+++ app/Models/Pendaftaran.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Pendaftaran extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'uuid',
+        'kode',
+        'email',
+        'nama',
+        'nohp_whatsapp',
+        'nohp_telegram',
+        'alamat',
+        'tgl_lahir',
+        'gender',
+        'domisili',
+        'angkatan',
+        'konfirmasi',
+        'tolak_tf',
+        'status',
+        'bukti_tf',
+        'waktu_konfirmasi',
+        'waktu_tolak_tf',
+        'Keterangan'
+    ];
+
+    public function scopeMenungguKonfirmasi($query)
+    {
+        return $query->where('status', '=', 1);
+    }
+
+    public function scopeAktif($query)
+    {
+        return $query->where('status', '=', 2);
+    }
+
+    public function scopeIkhwan($query)
+    {
+        return $query->where('gender', '=', 'L');
+    }
+
+    public function scopeAkhwat($query)
+    {
+        return $query->where('gender', '=', 'P');
+    }
+}
---
+++ database/migrations/2022_01_22_100407_create_pendaftarans_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePendaftaransTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pendaftarans', function (Blueprint $table) {
+            $table->id();
+            $table->uuid('uuid');
+            $table->string('kode');
+            $table->string('email');
+            $table->string('nama');
+            $table->string('nohp_whatsapp');
+            $table->string('nohp_telegram');
+            $table->text('alamat')->nullable();
+            $table->string('tgllahir')->nullable();
+            $table->string('gender')->nullable(); // L - IKHWAN, P - AKHWAT
+            $table->string('domisili')->nullable();
+            $table->string('angkatan')->nullable();
+            $table->integer('konfirmasi')->default(0); // JUMLAH KIRIM NOTIF KONFIRMASI
+            $table->integer('tolaktf')->default(0); // JUMLAH KIRIM NOTIF MENOLAK BUKTI TF
+            $table->integer('status')->default(0); // 1 DAFTAR/MENUNGGU KONFIRMASI - 2 AKTIF - 3 TOLAK TF
+            $table->string('bukti_tf')->nullable();
+            $table->string('waktu_konfirmasi')->nullable();
+            $table->string('waktu_tolaktf')->nullable();
+            $table->text('keterangan')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pendaftarans');
+    }
+}
